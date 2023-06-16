aed1f21772cf3ccc17006ccb7a57938258c49b81
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-15 19:12:14 -0500

    Update main.py
    Veamos si funciona

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 
 df1 = pd.read_parquet('dataset/archivo1.parquet')
 df2 = pd.read_parquet('dataset/archivo2.parquet')
@@ -20,16 +21,38 @@ def recomendar_sitios(state: str = None, categoria: str = None):
     devolver = ['name', 'address', 'avg_rating', 'categories', 'attributes']
     df_4 = df_3[devolver]
 
+    # Obtener las coordenadas del primer sitio con una puntuación de 5 estrellas
+    sitio_referencia = df_3[df_3['avg_rating'] == 5].iloc[0]
+    latitud_referencia = sitio_referencia['latitude']
+    longitud_referencia = sitio_referencia['longitude']
+
+    # Crear una instancia de NearestNeighbors
+    neigh = NearestNeighbors(n_neighbors=k)
+
+    # Ajustar el modelo a los datos de latitud y longitud
+    X = df_3[['latitude', 'longitude']].values
+    neigh.fit(X)
+
+    # Encontrar los k vecinos más cercanos a un punto en particular
+    distances, indices = neigh.kneighbors([[latitud_referencia, longitud_referencia]])
+
+    # Calcular la similitud del coseno
     features = ['latitude', 'longitude', 'avg_rating', 'review_count']
     X = df_3[features].values
-
     Similitud = cosine_similarity(X)
 
-    Indice = 0
-    Sitios_indice = Similitud[Indice].argsort()[:-11:-1]
+    # Combinar las puntuaciones de similitud del coseno y kneighbors
+    peso_coseno = 0.5
+    peso_kneighbors = 0.5
+    Similitud_combinada = peso_coseno * Similitud + peso_kneighbors * (1 - distances / distances.max())
+
+    # Obtener los sitios recomendados
+    Sitios_indice = Similitud_combinada[Indice].argsort()[:-11:-1]
     Sitios_similares = df_4.iloc[Sitios_indice]
 
     # Para que este ordenado por sitio
     lista_sitios = Sitios_similares.to_dict(orient='records')
 
     return lista_sitios[:5]
+
+
